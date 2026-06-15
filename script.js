e8d24ac7fe670c48231772a39b9778b6442502ee
frank0900768816-ui <frank0900768816@gmail.com>
2026-06-15 15:22:22 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ function toggleReview(title) {
 }
 
 
-
 //新增評論
 function addReview(){
     let star = document.querySelector('input[name="rating"]:checked');
@@ -24,10 +23,51 @@ function addReview(){
         return;
     }
 
-    // 模擬登入會員
-    let name="王小美";
+
+    // 新增：資料傳輸至置物櫃 LocalStorage
+    // 1. 撈出網頁上的商品名稱
+    // 檢查畫面上到底有沒有 id 叫 "productName" 的標籤：
+    // -> 如果「有」，就抓取它裡面的 innerText (文字)；
+    // -> 如果「沒有」，就自動用 "經典原木質感衣架(10入)" 當作預設商品名。
+    let pName = document.getElementById("productName") ? document.getElementById("productName").innerText : "經典原木質感衣架(10入)";
+    
+    // 2. 打包要傳到的會員中心的reviewData(評論)
+    // 把這一筆評論需要的所有資訊，集中包裝成一個方便傳輸的 JavaScript 物件。
+    let reviewData = {
+        productName: pName,
+        rating: star.value, // 傳送使用者點選的星等數字 (1~5)
+        comment: comment    // 傳送評論文字內容
+    };
+
+    // 3. 取出目前的舊評論陣列，如果沒東西，就給一個新陣列 []
+    // LocalStorage 類似共享資料庫，用 getItem('userReviews') 去拿貼有這個標籤的箱子。
+    // 資料庫只能存純文字，必須用 JSON.parse() 還原成 JavaScript 看得懂的「陣列 []」。
+    let currentReviews = JSON.parse(localStorage.getItem('userReviews')) || [];
+
+    // 4. 把這筆新發表的評論資料塞進陣列中
+    // 使用 push() 指令，把剛剛在第 2 步打包好資料 (reviewData)，塞進剛才拿出來的舊評論大陣列裡面。
+    // #push() 會把新評論排在最後面
+    currentReviews.push(reviewData);
+
+    // 5. 重新裝箱放回資料庫
+    // 所以在塞回去之前，必須用 JSON.stringify() 把整組陣列重新黏成「JSON 純文字字串」，
+    // 最後用 setItem 塞回 'userReviews' 的櫃位裡，這樣才算大功告成！
+    localStorage.setItem('userReviews', JSON.stringify(currentReviews));
+    // ==========================================
+
+
+    // 去 LocalStorage 檢查有沒有會員登入資料，有的話就抓名字，沒有才用預設名稱
+    let savedUser = localStorage.getItem('loggedInUser');
+    let name = "王小美"; // 預設名稱
+    if (savedUser) {
+        let userObj = JSON.parse(savedUser);
+        name = userObj.name; // 成功拿到 membership.html 傳過來的會員姓名
+    }
+
     let date = new Date().toLocaleDateString();
-    let number = 6 - star.value;
+    
+    // 改回點選幾星就秀幾顆星
+    let number = Number(star.value);
     let review = document.createElement("div");
 
     review.className="review";
@@ -83,8 +123,6 @@ function minus(){
     document.getElementById("num").innerHTML=num;
 
 }
-
-
 
 
 function cartClick() {
